f7a044ef812e28a945efb23336dda9cbd7298644
William <emersonwillian2020@gmail.com>
2024-10-16 15:38:44 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+mobileMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
